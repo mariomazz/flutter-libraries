27e6b3639fa70b4b44b7dab3292c2b2fb7310654
mario <mario.mazzarelli@asfweb.net>
2022-05-18 09:55:50 +0200

    ...

--- extended_methods_list/lib/extended_methods_list.dart
+++
@@ -1,9 +0,0 @@
-library extended_methods_list;
-
-import 'dart:math';
-
-/// A Calculator.
-class ExtendedMethodsList {
-  static T getRandomElement<T>(List<T> list) =>
-      list[Random().nextInt(list.length)];
-}
---
+++ extension_methods/lib/extension_methods.dart
@@ -0,0 +1,28 @@
+library extension_methods;
+
+import 'dart:math';
+
+extension ExString on String {
+  bool equals(String other) {
+    return this == other;
+  }
+
+  bool notEquals(String other) {
+    return this != other;
+  }
+
+  bool equalsAny(List<String> other) {
+    for (var value in other) {
+      if (equals(value)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
+
+extension ExList<T> on List<T> {
+  T randomItem() => this[Random().nextInt(length)];
+  List<T> shuffle() => this..shuffle();
+}
