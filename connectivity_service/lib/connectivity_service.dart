3ccfb55d684a6d26e510b2d54f566a5bb9df29cd
mario <mario.mazzarelli@asfweb.net>
2022-05-24 13:26:29 +0200

    ..

--- connectivity_service/lib/connectivity_service.dart
+++ connectivity_service/lib/connectivity_service.dart
@@ -1,7 +1,6 @@
 library connectivity_service;
 
 import 'dart:async';
-
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:rxdart/rxdart.dart';
 
@@ -23,22 +22,39 @@ class ConnectivityService {
 
   final Connectivity _connectivity = Connectivity();
 
-  final StreamController<ConnectivityResult> _streamController =
-      BehaviorSubject<ConnectivityResult>();
+  final StreamController<ConnectivityResultCS> _streamController =
+      BehaviorSubject<ConnectivityResultCS>();
 
-  Stream<ConnectivityResult> get stream => _streamController.stream;
+  Stream<ConnectivityResultCS> get stream => _streamController.stream;
 
   void _init() {
     _connectivity.onConnectivityChanged.listen((value) {
-      _streamController.add(value);
+      _streamController.add(_from(value));
     });
 
     _connectivity.checkConnectivity().then((value) {
-      _streamController.add(value);
+      _streamController.add(_from(value));
     });
   }
 
   void disose() {
     _streamController.close();
   }
+
+  ConnectivityResultCS _from(ConnectivityResult result) {
+    switch (result) {
+      case ConnectivityResult.bluetooth:
+        return ConnectivityResultCS.bluetooth;
+      case ConnectivityResult.ethernet:
+        return ConnectivityResultCS.ethernet;
+      case ConnectivityResult.wifi:
+        return ConnectivityResultCS.wifi;
+      case ConnectivityResult.mobile:
+        return ConnectivityResultCS.mobile;
+      default:
+        return ConnectivityResultCS.none;
+    }
+  }
 }
+
+enum ConnectivityResultCS { bluetooth, wifi, ethernet, mobile, none }
