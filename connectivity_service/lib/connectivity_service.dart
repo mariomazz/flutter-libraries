9c6bde887513698eb6ebbc23fb432052d03072ad
mario <mario.mazzarelli@asfweb.net>
2022-06-09 13:09:15 +0200

    ...

--- connectivity_service/lib/connectivity_service.dart
+++ connectivity_service/lib/connectivity_service.dart
@@ -60,7 +60,8 @@ class ConnectivityService {
 
   static Future<ConnectivityResultCS> checkConnectivity() async {
     try {
-      final result = await InternetAddress.lookup('example.com');
+      final result = await InternetAddress.lookup(
+          'www.google.com'); // google address for validity issues
       if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
         return await _connectivity
             .checkConnectivity()
