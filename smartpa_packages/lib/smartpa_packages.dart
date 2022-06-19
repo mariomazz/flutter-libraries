8baff121bcb72c095b1e2f1654d988e279bdaa3f
mario <mario.mazzarelli@asfweb.net>
2022-06-19 22:29:15 +0200

    ..

--- smartpa_packages/lib/smartpa_packages.dart
+++ smartpa_packages/lib/smartpa_packages.dart
@@ -1,8 +1,8 @@
 library smartpa_packages;
 
-export "package:api_controller/api_controller.dart";
+/* export "package:api_controller/api_controller.dart";
 export "package:authentication/authentication.dart";
 export "package:widgets/widgets.dart";
 export "package:routing_gr/routing_gr.dart";
-export "package:extension_methods/extension_methods.dart";
+export "package:extension_methods/extension_methods.dart"; */
 
